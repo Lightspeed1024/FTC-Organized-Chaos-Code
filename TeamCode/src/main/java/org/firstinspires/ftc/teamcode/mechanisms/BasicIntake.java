461ca92d9404bdc6acc72fe43f0f221cc680c3b6
Mystic Owl <qihanzhang2025@gmail.com>
2026-09-06 11:18:41 -0700

    Added intake code as a seperate file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/BasicIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/BasicIntake.java
@@ -10,8 +10,8 @@ public class BasicIntake {
     public void init(HardwareMap hwMap) {
         intake = hwMap.get(DcMotor.class, "intake");
 
-        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         intake.setDirection(DcMotor.Direction.FORWARD);
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
